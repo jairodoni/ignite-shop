3cdd0cf1ce372a915868cbfc6205698de19a5c77
Jairo Doni <jairo.doni97@gmail.com>
2023-08-15 16:23:49 -0300

    Design da Home e Produtos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
+
+import { globalStyles } from '@/styles/global'
+import { Container, Header } from '../styles/pages/app'
+
+import logoImg from '@/assets/imgs/logo.svg'
+import Image from 'next/image'
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-roboto',
+})
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,56 @@
-import { Inter } from 'next/font/google'
+import { useKeenSlider } from 'keen-slider/react'
+import { HomeContainer, Product } from '../styles/pages/home'
+import Image from 'next/image'
 
-const inter = Inter({ subsets: ['latin'] })
+import shirt01 from '@/assets/imgs/Shirt-1.png'
+import shirt02 from '@/assets/imgs/Shirt-2.png'
+import shirt03 from '@/assets/imgs/Shirt-3.png'
+import shirt04 from '@/assets/imgs/Shirt-4.png'
+
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+
   return (
-    <>
-      <h1>BATATA</h1>
-    </>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={shirt04} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Lorem Ipsum is simply dummy</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt03} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Lorem Ipsum is simply dummy</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt02} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Lorem Ipsum is simply dummy</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={shirt01} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Lorem Ipsum is simply dummy</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
