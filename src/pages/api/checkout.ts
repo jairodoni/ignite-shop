fdc3b08b588d28684a8931143d6542b4ff90c6c1
Jairo Doni <jairo.doni97@gmail.com>
2023-08-28 18:28:45 -0300

    Add opção de preencher endereço de entrega e frete personalizado

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -23,6 +23,51 @@ export default async function handler(
     cancel_url: cancelUrl,
     mode: 'payment',
     line_items: listCartProducts,
+    shipping_address_collection: {
+      allowed_countries: ['BR'],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 0,
+            currency: 'brl',
+          },
+          display_name: 'Frete Gratis',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 5,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 30,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 10000,
+            currency: 'brl',
+          },
+          display_name: 'Entrega expressa',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 1,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 1,
+            },
+          },
+        },
+      },
+    ],
   })
   return res.status(201).json({
     checkoutUrl: [checkoutSession.url],
